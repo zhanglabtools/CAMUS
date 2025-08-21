0f2c52fdb745f799941aa316abd86f08a3106629
Smilenone <30334073+Smilenone@users.noreply.github.com>
2025-08-21 14:45:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import Command, find_packages, setup
 
 setup(
     name = "CAMUS",
-    version = "1.0.0",
+    version = "1.0.1",
     description = "Cross-datasets annotation modeling with universal reference data and method selection",
     url = "https://github.com/Zhanglabtools/CAMUS",
     author = "Qunlun Shen",
@@ -14,3 +14,4 @@ setup(
     zip_safe = False,
     include_package_data = True,
 )
+
